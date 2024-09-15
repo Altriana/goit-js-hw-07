286c197f5c8b619a51c62923e845167ea6e3a947
Altriana <150026999+Altriana@users.noreply.github.com>
2024-09-15 23:46:31 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector(".change-color");
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  let color = document.querySelector(".color");
+  color.textContent = document.body.style.backgroundColor;
+})
