f6cc20658269266299a5c04138b84a4b92a8c97d
Altriana <150026999+Altriana@users.noreply.github.com>
2024-09-15 23:24:30 +0200

    Upgrade task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+    
+    if (email === "" || password === "") {
+    return alert("All form fields must be filled in");
+    };
+
+    const data = {
+                email: email,
+                password: password
+            };
+
+    console.log(data);
+    form.reset();
+});
 
