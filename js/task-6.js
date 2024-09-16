361457dffe957f0ff79e1ee6f64f714ae61e1b64
Altriana <150026999+Altriana@users.noreply.github.com>
2024-09-16 12:27:11 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,50 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("#controls input");
+const create = document.querySelector("button[data-create]");
+const destroy = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+const createBoxes = (amount) => {
+        
+        let size = 30;
+        const ratio = 10;
+        
+        const newBoxes = Array.from({ length: amount }, (_, i) => {
+        const currentSize = `${size + (i + boxes.children.length) * ratio}px`;  
+          
+            const box = document.createElement("div");
+            box.style.width = currentSize;
+            box.style.height = currentSize;
+            box.style.backgroundColor = getRandomHexColor();
+
+            return box;
+        });
+
+        const fragment = document.createDocumentFragment();
+        fragment.append(...newBoxes);
+
+        boxes.prepend(fragment);
+};
+    
+ function destroyBoxes() {
+            boxes.innerHTML = "";
+        }
+
+        
+create.addEventListener('click', () => {
+    const amount = parseInt(input.value, 10);
+
+    if (amount >= 1 && amount <= 100) {
+        createBoxes(amount);
+      input.value = "";
+    } else {
+        
+    }
+});
+
+destroy.addEventListener('click', () => {
+  destroyBoxes();
+});
